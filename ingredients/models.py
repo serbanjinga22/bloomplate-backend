614a96ed62273272699f8d579ece70e200e328f6
SerbanJinga <33983121+SerbanJinga@users.noreply.github.com>
2024-06-12 20:08:12 +0200

    final push

--- ingredients/models.py
+++ ingredients/models.py
@@ -3,19 +3,28 @@ from django.db import models
 # Create your models here.
 class Ingredient(models.Model):
     UNIT_CHOICES = [
-        ('g', 'Grams'),
-        ('kg', 'Kilograms'),
-        ('ml', 'Milliliters'),
-        ('l', 'Liters'),
-        ('tsp', 'Teaspoons'),
-        ('tbsp', 'Tablespoons'),
-        ('cup', 'Cups'),
-        ('piece', 'Pieces'),
+        ('g', 'Grame'),
+        ('kg', 'Kilograme'),
+        ('ml', 'Mililitri'),
+        ('l', 'Litri'),
+        ('tsp', 'Lingurițe'),
+        ('tbsp', 'Linguri'),
+        ('cup', 'Căni'),
+        ('piece', 'Bucăți'),
+    ]
+
+    INGREDIENT_TYPE_CHOICES = [
+        ('Lactate, brânză și ouă', 'Lactate, brânză și ouă'),
+        ('Legume și fructe', 'Legume și fructe'),
+        ('Carne și pește', 'Carne și pește'),
+        ('Cereale, făinoase, nuci', 'Cereale, făinoase, nuci'),
+        ('Condimente și Uleiuri', 'Condimente și Uleiuri'),
+        ('Diverse', 'Diverse'),
     ]
     name = models.CharField(max_length=255, blank=False)
     unit_of_measure = models.CharField(max_length=255, blank=False, choices=UNIT_CHOICES, default='g')
     quantity = models.IntegerField(default=0)
-    ingredientType = models.CharField(max_length=255, blank=True)
+    ingredientType = models.CharField(max_length=255, blank=True, choices=INGREDIENT_TYPE_CHOICES, default='Diverse')
 
     def __str__(self):
         return self.name
